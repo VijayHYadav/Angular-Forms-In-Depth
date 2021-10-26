73356efcf4366694e2c4211365dbb70e3ec8643a
VijayHYadav <vizzyadav1997@gmail.com>
2021-10-27 05:27:10 +0530

    15. Understanding ngModel One Way and Bidirectional Data Binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,6 +9,10 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  val = {
+    email: 'hello@gmail.com',
+    password: '123456'
+  };
 
   constructor() {
 
@@ -21,6 +25,9 @@ export class LoginComponent implements OnInit {
 
   login(loginForm: NgForm, submit: any) {
     console.log(loginForm.value, loginForm.valid, submit);
+
+    console.log("val", this.val);
+    
   }
 
   onEmailChange(change) {
