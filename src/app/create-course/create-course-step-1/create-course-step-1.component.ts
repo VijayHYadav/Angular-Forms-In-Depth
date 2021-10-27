8a3c583a5e2f19a584d91af1dbfa188554479af9
VijayHYadav <vizzyadav1997@gmail.com>
2021-10-28 04:55:58 +0530

    32. Angular Selection Dropdown Form Control - Step-by-Step Example

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -5,6 +5,11 @@ import {Observable} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import { courseTitleValidator } from '../../validators/course-title.validator';
 
+interface CourseCategory {
+  code: string;
+  description: string;
+}
+
 @Component({
   selector: 'create-course-step-1',
   templateUrl: './create-course-step-1.component.html',
@@ -23,16 +28,19 @@ export class CreateCourseStep1Component implements OnInit {
       updateOn: 'blur'
     }],
     releaseAt: [new Date(), Validators.required],
+    category: ['BEGINNER', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
     longDescription: ['', [Validators.required, Validators.minLength(3)]]
   })
+
+  courseCategories$: Observable<CourseCategory>
 
   constructor(private fb: FormBuilder, private course: CoursesService) {
 
   }
 
   ngOnInit() {
-
+    this.courseCategories$ = this.course.findCourseCategories();
   }
 
   get courseTitle() {
