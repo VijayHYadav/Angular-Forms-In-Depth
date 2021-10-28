69b961c82d5990ac4cd055c7431e0cf544c47fac
VijayHYadav <vizzyadav1997@gmail.com>
2021-10-29 02:55:44 +0530

    48. How to Use ControlValueAccessor to implement a Nested Form

--- src/app/address-form/address-form.component.ts
+++ src/app/address-form/address-form.component.ts
@@ -13,23 +13,60 @@ import {noop, Subscription} from 'rxjs';
 @Component({
   selector: 'address-form',
   templateUrl: './address-form.component.html',
-  styleUrls: ['./address-form.component.scss']
+  styleUrls: ['./address-form.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: AddressFormComponent
+    }
+  ]
 })
-export class AddressFormComponent {
+export class AddressFormComponent implements ControlValueAccessor, OnDestroy{
 
     @Input()
     legend:string;
+
+    onTouched = () => {};
+
+    onChangeSub: Subscription;
 
     form: FormGroup = this.fb.group({
         addressLine1: [null, [Validators.required]],
         addressLine2: [null, [Validators.required]],
         zipCode: [null, [Validators.required]],
-        city: [null, [Validators.required]]
+        city: [null, [Validators.required]],
     });
 
     constructor(private fb: FormBuilder) {
+
     }
 
+    registerOnChange(onChange: any) {
+      this.onChangeSub = this.form.valueChanges.subscribe(onChange);
+    }
+
+    ngOnDestroy() {
+      this.onChangeSub.unsubscribe();
+    }
+
+    writeValue(value: any) {
+      if(value) {
+        this.form.setValue(value);
+      }
+    }
+
+    registerOnTouched(onTouched: any) {
+      this.onTouched = onTouched;
+    }
+
+    setDisabledState(disabled: boolean) {
+      if(disabled) {
+        this.form.disable();
+      } else {
+        this.form.enable();
+      }
+    }
 }
 
 
